15d558e44af071974a87f8d4d2f65e4bb773b8b7
Matic Zavadlal <matic.zavadlal@gmail.com>
2018-10-20 11:35:58 +0200

    Webhook init

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import { importSchema } from 'graphql-import'
 import { resolvers, fragmentReplacements } from './resolvers'
 
 import { getNeo4JSession } from './neo4j'
+import { Slack } from './slack'
 
 const typeDefs = gql(importSchema('./src/schema.graphql'))
 
@@ -14,6 +15,10 @@ const server = new ApolloServer({
   context: req => ({
     ...req,
     neo4j: getNeo4JSession(),
+    slack: new Slack({
+      organization: process.env.SLACK_ORGANIZATION,
+      apiToken: process.env.SLACK_API_TOKEN,
+    }),
   }),
 })
 
--- webhook/src/index.ts
+++ webhook/src/index.ts
@@ -1,1 +1,20 @@
-console.log('Heo')
+import * as express from 'express'
+
+// Config validation
+
+if (!process.env.PORT) {
+  throw new Error(`Missing PORT environment variable.`)
+}
+
+// Webhook
+
+const webhook = express()
+
+webhook.use('/')
+
+// Start
+
+webhook.listen(process.env.PORT, err => {
+  if (err) throw err
+  console.log(`Webhook listening on: http://localhost:${process.env.PORT}`)
+})
